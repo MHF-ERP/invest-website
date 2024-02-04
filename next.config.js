d494a1b141a5a8492e65cd2d5fd82f90df8cabf7
jokerf6 <fhakem75@gmail.com>
2024-02-04 11:11:54 -0800

    solve image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["127.0.0.1"], // Add the hostname(s) here
+    domains: ["api.naqiconcepts.com"], // Add the hostname(s) here
   },
 };
