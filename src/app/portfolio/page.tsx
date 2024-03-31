6ad0b25453bf5b3b6a2cbad209620699ae2ac3be
jokerf6 <fhakem75@gmail.com>
2024-03-31 20:36:10 +0300

    solve all

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -73,7 +73,7 @@ export default function Page() {
                 <Actions />
               </div>
             </div>
-            <Changes />
+            {/* <Changes /> */}
           </HomeLayout>
         )}
       </main>
